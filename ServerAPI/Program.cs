c5a6749a9b8d338c14896d65556203fc4c8e9833
김준우 <kjw991953@gmail.com>
2026-03-13 14:31:56 +0900

    swagger 연동 로직 추가

--- ServerAPI/Program.cs
+++ ServerAPI/Program.cs
@@ -2,19 +2,27 @@ using Microsoft.EntityFrameworkCore;
 using ServerApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // 1. 설정 파일에서 연결 문자열(주소) 가져오기
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // 2. DbContext 등록 (MySQL 연결 설정)
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Game Server API v1");
+    // 접속 기본 경로를 swagger로 설정 (필요시)
+    c.RoutePrefix = string.Empty;
+});
 
 // 서버 구동 시 DB 테이블 자동 생성 스크립트
 using (var scope = app.Services.CreateScope())
@@ -23,12 +31,7 @@ using (var scope = app.Services.CreateScope())
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     dbContext.Database.EnsureCreated();
 }
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseRouting(); // [추가 권장] 명시적으로 라우팅 활성화
 
 app.UseAuthorization();
 
