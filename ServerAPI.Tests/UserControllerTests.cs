3c65ae4eb3704ae2b1b4d0341a3af242a3354de4
김준우 <kjw991953@gmail.com>
2026-03-13 17:18:06 +0900

    단위테스트 추가/ github actions에서 단위테스트 검증

---
+++ ServerAPI.Tests/UserControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using ServerApi.Data;
+using ServerApi.Models;
+using ServerAPI.Controllers;
+using Xunit;
+
+public class UserControllerTests
+{
+    private AppDbContext GetDatabaseContext()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        var databaseContext = new AppDbContext(options);
+        databaseContext.Database.EnsureCreated();
+        return databaseContext;
+    }
+
+    [Fact]
+    public async Task RegisterUser_ReturnsOk_WhenNicknameIsNew()
+    {
+        // Arrange (준비)
+        var context = GetDatabaseContext();
+        var controller = new UserController(context);
+        var nickname = "NewPlayer";
+
+        // Act (실행)
+        var result = await controller.RegisterUser(nickname);
+
+        // Assert (검증)
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnedUser = Assert.IsType<User>(okResult.Value);
+        Assert.Equal(nickname, returnedUser.Nickname);
+    }
+}
