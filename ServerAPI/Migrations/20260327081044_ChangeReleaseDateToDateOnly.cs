fa95e3743870c944d127e84e797681d3019925f5
김준우 <kjw991953@gmail.com>
2026-03-27 17:11:33 +0900

    마이그레이션 내역 추가

---
+++ ServerAPI/Migrations/20260327081044_ChangeReleaseDateToDateOnly.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeReleaseDateToDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "release_date",
+                table: "game_versions",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "release_date",
+                table: "game_versions",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
